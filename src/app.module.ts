1580357e9a2e6600e860f6a36aa9c2c566ff7e4b
SazukeR <reinaldojcg1@gmail.com>
2024-03-27 19:24:46 -0500

    task sntity with typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { TasksModule } from './tasks/tasks.module';
 
 
 @Module({
@@ -16,6 +17,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       autoLoadEntities: true,
       synchronize: true,
     }),
+    TasksModule,
   ],
 
 })
